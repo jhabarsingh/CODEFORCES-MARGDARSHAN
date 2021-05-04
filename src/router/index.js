73ab28e682b9a9d43237f99096096e4deeab7911
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-05 05:06:16 +0530

    changed route name

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
+    path: '/submissions',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
