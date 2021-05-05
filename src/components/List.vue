58532c324d66aff417d1d26ac4a3268214494ac3
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-05 06:11:04 +0530

    Added list component

---
+++ src/components/List.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="700"
+    :elevation="2"
+  >
+    <v-list>
+      <v-list-item-group v-model="model">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-icon>        
+            <v-icon
+            md
+            color="green darken-2"
+            >
+            <!-- mdi-checkbox-marked-circle -->
+            mdi-pencil
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          icon: 'mdi-inbox',
+          text: 'Inbox',
+        },
+        {
+          icon: 'mdi-star',
+          text: 'Star',
+        },
+        {
+          icon: 'mdi-send',
+          text: 'Send',
+        },
+        {
+          icon: 'mdi-email-open',
+          text: 'Drafts',
+        },
+      ],
+      model: 1,
+    }),
+  }
+</script>
